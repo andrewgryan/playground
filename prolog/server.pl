9f4d21dd45f28a9e655fd9b72336db3036e744e4
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-10-19 20:32:38 +0100

    return JSON

--- prolog/server.pl
+++ prolog/server.pl
@@ -1,8 +1,10 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_json)).
 :- use_module(library(http/html_write)).
 
 :- http_handler(root(hello_world), say_hi, []).
+:- http_handler(root(data), get_data, []).
 
 server(Port) :-
     http_server(http_dispatch, [port(Port)]).
@@ -10,3 +12,6 @@ server(Port) :-
 say_hi(_Request) :-
     reply_html_page(title('Hello, World!'),
                     [ h1('Hello, World!'), p('A paragraph...') ]).
+
+get_data(_Request) :-
+    reply_json(json([hello='world'])).
