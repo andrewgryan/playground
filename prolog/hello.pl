00d3808e6d1ad4a921e39ad38fa9d8d2e012660c
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-10-19 19:05:14 +0100

    add comment to remind myself how to run prolog programs

--- prolog/hello.pl
+++ prolog/hello.pl
@@ -1,4 +1,12 @@
 :- initialization main, halt.
 
+/**
+ * Hello, World!
+ *
+ * Call from command line with swipl hello.pl or
+ * from prolog interpreter with [hello]
+ *
+ */
+
 main :-
     write('Hello, World!'), nl.
