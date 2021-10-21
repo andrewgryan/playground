62e2d0970b4db183d152c37f882ff453bd34b93c
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-10-21 10:06:12 +0100

    upgrade react app to display tictactoe winner

--- prolog/app.jsx
+++ prolog/app.jsx
@@ -3,28 +3,36 @@ import ReactDOM from "react-dom"
 
 
 const App = () => {
-    const [ board, setBoard ] = useState(new Array(9).fill("b"))
-    return (<div class="grid grid-cols-3">{
-        board.map((content, i) => {
-            const onClick = () => {
-                const url = `/tictactoe?move=${i}&board=${board.join('')}`
-                fetch(url)
-                    .then(resp => resp.json())
-                    .then(({ board }) => {
-                        if (typeof board !== "undefined") {
-                            setBoard(board)
-                        }
-                    })
+    const blank = "b"
+    const [ board, setBoard ] = useState(new Array(9).fill(blank))
+    const [ winner, setWinner ] = useState(blank)
+    return (
+    <div>
+        { winner !== blank && <div>Winner: { winner }</div> }
+        <div class="grid grid-cols-3">
+            { board.map((content, i) => {
+                const onClick = () => {
+                    const url = `/tictactoe?move=${i}&board=${board.join('')}`
+                    fetch(url)
+                        .then(resp => resp.json())
+                        .then(({ board, winner }) => {
+                            if (typeof board !== "undefined") {
+                                setBoard(board)
+                            }
+                            setWinner(winner)
+                        })
+                }
+                let mark
+                if (content === blank) {
+                    mark = ""
+                } else {
+                    mark = content
+                }
+                return <Tile key={ i } onClick={ onClick }>{ mark }</Tile>
+            })
             }
-            let mark
-            if (content === "b") {
-                mark = ""
-            } else {
-                mark = content
-            }
-            return <Tile key={ i } onClick={ onClick }>{ mark }</Tile>
-        })
-        }</div>)
+        </div>
+    </div>)
 }
 
 
