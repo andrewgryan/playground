2df03a7ed86abda8b5a279a21f7eb27ed1d73a8b
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-10-26 11:30:14 +0100

    tail recursive factorial

--- erlang/useless.erl
+++ erlang/useless.erl
@@ -1,6 +1,12 @@
 -module(useless).
--export([hello_world/0]).
+-export([hello_world/0, factorial/1]).
 
 
+% Obligatory Hello, World! function
 hello_world() ->
     io:format("Hello, World!~n").
+
+
+% Traditional factorial function
+factorial(0) -> 1;
+factorial(N) when N > 0 -> N * factorial(N - 1).
