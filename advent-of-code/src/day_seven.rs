b73c71e9c31d22b869668c568ba35f75a87474ce
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-12-11 21:29:52 +0000

    solution to day 7 part 2

--- advent-of-code/src/day_seven.rs
+++ advent-of-code/src/day_seven.rs
@@ -5,7 +5,8 @@ pub fn part_one(input_file: &str) -> i32 {
     let content = fs::read_to_string(input_file).unwrap();
     let row = content.split('\n').next().unwrap();
     let positions: Vec<i32> = row.split(',').map(|s| s.parse().unwrap()).collect();
-    match minimum_position(positions) {
+    let cost_fn = quadratic_cost; // linear_cost for part 1
+    match minimum_position(positions, cost_fn) {
         None => -1,
         Some(place) => place.cost,
     }
@@ -22,20 +23,20 @@ impl Position {
     }
 }
 
-pub fn minimum_fuel(positions: Vec<i32>) -> i32 {
-    match minimum_position(positions) {
+pub fn minimum_fuel(positions: Vec<i32>, cost_fn: fn(&i32, i32) -> i32) -> i32 {
+    match minimum_position(positions, cost_fn) {
         None => -1,
         Some(place) => place.x,
     }
 }
 
-pub fn minimum_position(positions: Vec<i32>) -> Option<Position> {
+pub fn minimum_position(positions: Vec<i32>, cost_fn: fn(&i32, i32) -> i32) -> Option<Position> {
     let mut position: Option<Position> = None;
     let start = positions.iter().min().unwrap();
     let end = positions.iter().max().unwrap();
     for x in *start..(*end + 1) {
         println!("{}", x);
-        let new_position = Position::new(x, cost(&positions, x));
+        let new_position = Position::new(x, cost(&positions, x, cost_fn));
         match position {
             None => position = Some(new_position),
             Some(old_position) => {
@@ -48,12 +49,22 @@ pub fn minimum_position(positions: Vec<i32>) -> Option<Position> {
     position
 }
 
-pub fn cost(positions: &Vec<i32>, x: i32) -> i32 {
+pub fn cost(positions: &Vec<i32>, x: i32, cost_fn: fn(&i32, i32) -> i32) -> i32 {
     let mut result = 0;
     for position in positions {
-        result += (position - x).abs();
+        result += cost_fn(position, x);
     }
     result
+}
+
+pub fn linear_cost(start: &i32, end: i32) -> i32 {
+    (start - end).abs()
+}
+
+pub fn quadratic_cost(start: &i32, end: i32) -> i32 {
+    // 4 -> 1 + 2 + 3 + 4 = 10 -> n * (n + 1) / 2
+    let n = (start - end).abs();
+    n * (n + 1) / 2
 }
 
 #[cfg(test)]
@@ -66,16 +77,25 @@ mod tests {
     #[case(2, 37)]
     #[case(3, 39)]
     #[case(10, 71)]
-    fn day_seven_cost(#[case] input: i32, #[case] expected: i32) {
+    fn day_seven_cost_given_linear_cost(#[case] input: i32, #[case] expected: i32) {
         let data = vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
-        let actual = cost(&data, input);
+        let actual = cost(&data, input, linear_cost);
+        assert_eq!(actual, expected);
+    }
+
+    #[rstest]
+    #[case(2, 206)]
+    #[case(5, 168)]
+    fn day_seven_cost_given_quadratic_cost(#[case] input: i32, #[case] expected: i32) {
+        let data = vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+        let actual = cost(&data, input, quadratic_cost);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn day_seven_minimum_position() {
         let data = vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
-        let actual = minimum_fuel(data);
+        let actual = minimum_fuel(data, linear_cost);
         assert_eq!(actual, 2);
     }
 }
