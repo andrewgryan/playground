33ef2ec783ea3212fc03db8dc220d9b4cf97bba4
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-12-31 13:20:06 +0000

    implement turn system

--- advent-of-code/src/day_eleven.rs
+++ advent-of-code/src/day_eleven.rs
@@ -8,6 +8,33 @@ pub fn part_one(puzzle_input: &str) -> u32 {
     0
 }
 
+pub fn turn(array: &Array2<Octopus>) -> Array2<Octopus> {
+    let mut copy = array.to_owned();
+    turn_mut(&mut copy);
+    copy
+}
+
+pub fn turn_mut(array: &mut Array2<Octopus>) {
+    // Increase energy levels
+    array.map_mut(|octopus| *octopus += 1);
+
+    // Find charged octopuses
+    let mut indices = find_charged(&array);
+
+    while indices.len() > 0 {
+        // Flash charged octopuses
+        for index in indices.clone() {
+            flash_mut([index.0, index.1], array);
+        }
+
+        // Find next batch of octopuses
+        indices = find_charged(&array);
+    }
+
+    // Reset the spent octopus charges
+    array.map_mut(|octopus| octopus.reset_charge());
+}
+
 fn to_octopus_array(puzzle_input: &str, shape: (usize, usize)) -> Array2<Octopus> {
     let values: Vec<Octopus> = puzzle_input
         .split('\n')
@@ -20,6 +47,15 @@ fn to_octopus_array(puzzle_input: &str, shape: (usize, usize)) -> Array2<Octopus
         .flatten()
         .collect();
     Array2::from_shape_vec(shape, values).unwrap()
+}
+
+/// Find charged Octopus locations
+pub fn find_charged(array: &Array2<Octopus>) -> Vec<(usize, usize)> {
+    array
+        .indexed_iter()
+        .filter(|(_, octopus)| octopus.is_charged())
+        .map(|(index, _)| index)
+        .collect()
 }
 
 /// Flash Octopus return modified copy of array
@@ -83,12 +119,25 @@ impl Octopus {
         }
     }
 
+    pub fn is_charged(&self) -> bool {
+        !self.spent && self.energy_level > 9
+    }
+
     pub fn is_spent(&self) -> bool {
         self.spent
     }
 
     pub fn flash(&mut self) {
         self.spent = true
+    }
+
+    pub fn reset_charge(&mut self) {
+        // Reset charge state
+        if self.is_spent() {
+            self.energy_level = 0;
+        }
+        // Reset spent state
+        self.reset();
     }
 
     pub fn reset(&mut self) {
@@ -266,5 +315,115 @@ mod tests {
         octopus.flash();
         octopus.reset();
         assert!(!octopus.is_spent());
+    }
+
+    #[test]
+    fn find_charged_indices() {
+        let text: &str = "11111
+                          11911
+                          11111
+                          11111
+                          11111";
+        let mut grid: Array2<Octopus> = to_octopus_array(text, (5, 5));
+        grid = grid + 1;
+        let actual = find_charged(&grid);
+        let expected = vec![(1, 2)];
+        assert_eq!(actual, expected);
+        assert_eq!(grid[(1, 2)], Octopus::new(10));
+    }
+
+    #[rstest]
+    #[case("11111
+            19991
+            19191
+            19991
+            11111",
+            "34543
+             40004
+             50005
+             40004
+             34543", (5,5), 1)]
+    #[case("11111
+            19991
+            19191
+            19991
+            11111",
+            "45654
+             51115
+             61116
+             51115
+             45654", (5,5), 2)]
+    #[case("5483143223
+            2745854711
+            5264556173
+            6141336146
+            6357385478
+            4167524645
+            2176841721
+            6882881134
+            4846848554
+            5283751526",
+            "6594254334
+             3856965822
+             6375667284
+             7252447257
+             7468496589
+             5278635756
+             3287952832
+             7993992245
+             5957959665
+             6394862637", (10,10), 1)]
+    #[case("5483143223
+            2745854711
+            5264556173
+            6141336146
+            6357385478
+            4167524645
+            2176841721
+            6882881134
+            4846848554
+            5283751526",
+            "8807476555
+             5089087054
+             8597889608
+             8485769600
+             8700908800
+             6600088989
+             6800005943
+             0000007456
+             9000000876
+             8700006848", (10,10), 2)]
+    #[case("5483143223
+            2745854711
+            5264556173
+            6141336146
+            6357385478
+            4167524645
+            2176841721
+            6882881134
+            4846848554
+            5283751526",
+            "0397666866
+             0749766918
+             0053976933
+             0004297822
+             0004229892
+             0053222877
+             0532222966
+             9322228966
+             7922286866
+             6789998766", (10,10), 100)]
+    fn play_turns(
+        #[case] text: &str,
+        #[case] end: &str,
+        #[case] shape: (usize, usize),
+        #[case] n: u32,
+    ) {
+        let mut actual: Array2<Octopus> = to_octopus_array(text, shape);
+        for _ in 0..n {
+            turn_mut(&mut actual);
+        }
+        let expected: Array2<Octopus> = to_octopus_array(end, shape);
+        assert_eq!(actual, expected);
     }
 }
