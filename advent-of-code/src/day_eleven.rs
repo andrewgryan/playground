9d4ead0f53de637a7ca76079c48a5c228d71e144
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-12-31 10:06:11 +0000

    add internal flash state to prevent multiple flashes

--- advent-of-code/src/day_eleven.rs
+++ advent-of-code/src/day_eleven.rs
@@ -22,45 +22,74 @@ fn to_octopus_array(puzzle_input: &str, shape: (usize, usize)) -> Array2<Octopus
     Array2::from_shape_vec(shape, values).unwrap()
 }
 
+/// Flash Octopus return modified copy of array
 pub fn flash(index: [usize; 2], array: &Array2<Octopus>) -> Array2<Octopus> {
-    let [i, j] = index;
     let mut copy = array.to_owned();
-    // Corners
-    copy[[i - 1, j - 1]] += 1;
-    copy[[i - 1, j + 1]] += 1;
-    copy[[i + 1, j + 1]] += 1;
-    copy[[i + 1, j - 1]] += 1;
-
-    // Cross
-    copy[[i, j + 1]] += 1;
-    copy[[i, j - 1]] += 1;
-    copy[[i + 1, j]] += 1;
-    copy[[i - 1, j]] += 1;
-
+    flash_mut(index, &mut copy);
     copy
 }
 
-#[derive(Clone, Debug, PartialEq)]
+/// Flash Octopus array in-place
+pub fn flash_mut(index: [usize; 2], array: &mut Array2<Octopus>) {
+    let [i, j] = index;
+
+    let octopus = &mut array[[i, j]];
+    if !octopus.is_spent() {
+        // Flash octopus
+        octopus.flash();
+
+        // Corners
+        array[[i - 1, j - 1]] += 1;
+        array[[i - 1, j + 1]] += 1;
+        array[[i + 1, j + 1]] += 1;
+        array[[i + 1, j - 1]] += 1;
+
+        // Cross
+        array[[i, j + 1]] += 1;
+        array[[i, j - 1]] += 1;
+        array[[i + 1, j]] += 1;
+        array[[i - 1, j]] += 1;
+    }
+}
+
+#[derive(Clone, Debug, Default, PartialEq)]
 pub struct Octopus {
     energy_level: u32,
+    spent: bool,
 }
 
 impl Octopus {
     pub fn new(energy_level: u32) -> Self {
-        Self { energy_level }
+        Self {
+            energy_level,
+            ..Default::default()
+        }
+    }
+
+    pub fn is_spent(&self) -> bool {
+        self.spent
+    }
+
+    pub fn flash(&mut self) {
+        self.spent = true
+    }
+
+    pub fn reset(&mut self) {
+        self.spent = false
     }
 }
 
 impl AddAssign<u32> for Octopus {
     fn add_assign(&mut self, other: u32) {
-        self.energy_level += 1;
+        self.energy_level += other;
     }
 }
 impl Add<u32> for Octopus {
     type Output = Self;
     fn add(self, other: u32) -> Self::Output {
         Self {
-            energy_level: self.energy_level + 1,
+            energy_level: self.energy_level + other,
+            ..Default::default()
         }
     }
 }
@@ -114,14 +143,42 @@ mod tests {
                            00000";
         let array: Array2<Octopus> = to_octopus_array(start, (5, 5));
 
-        let actual = flash([1, 2], &array);
+        let [i, j] = [1, 2];
+        let actual = flash([i, j], &array);
 
         let end: &str = "01110
                          01010
                          01110
                          00000
                          00000";
-        let expected: Array2<Octopus> = to_octopus_array(end, (5, 5));
+        let mut expected: Array2<Octopus> = to_octopus_array(end, (5, 5));
+        expected[[i, j]].flash();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn octopus_flash_multiple_times_without_reset() {
+        let start: &str = "00000
+                           00000
+                           00000
+                           00000
+                           00000";
+        let mut actual: Array2<Octopus> = to_octopus_array(start, (5, 5));
+        let [i, j] = [1, 2];
+
+        // in-place mutable flash
+        flash_mut([i, j], &mut actual);
+        flash_mut([i, j], &mut actual);
+        flash_mut([i, j], &mut actual);
+        flash_mut([i, j], &mut actual);
+
+        let end: &str = "01110
+                         01010
+                         01110
+                         00000
+                         00000";
+        let mut expected: Array2<Octopus> = to_octopus_array(end, (5, 5));
+        expected[[i, j]].flash();
         assert_eq!(actual, expected);
     }
 
@@ -144,5 +201,21 @@ mod tests {
                          11111";
         let expected: Array2<Octopus> = to_octopus_array(end, (5, 5));
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn octopus_flash_internal_state() {
+        let mut octopus: Octopus = Octopus::new(0);
+        assert!(!octopus.is_spent());
+        octopus.flash();
+        assert!(octopus.is_spent());
+    }
+
+    #[test]
+    fn octopus_reset_internal_state() {
+        let mut octopus: Octopus = Octopus::new(0);
+        octopus.flash();
+        octopus.reset();
+        assert!(!octopus.is_spent());
     }
 }
