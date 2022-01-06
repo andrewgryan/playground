b6174f2af8871f3d4d80c557f439362777a1c1eb
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-01-06 17:38:24 +0000

    solution day 14 part one

--- advent-of-code/src/day_fourteen.rs
+++ advent-of-code/src/day_fourteen.rs
@@ -22,7 +22,15 @@ pub fn part_one(puzzle: &str) -> u32 {
 }
 
 #[derive(Debug, PartialEq)]
-struct Polymer(String);
+pub struct Polymer(String);
+
+impl Polymer {
+    pub fn to_string(&self) -> String {
+        match &self {
+            Polymer(s) => s.to_string(),
+        }
+    }
+}
 
 impl FromStr for Polymer {
     type Err = ();
@@ -40,7 +48,13 @@ impl FromStr for Polymer {
 }
 
 #[derive(Debug, PartialEq)]
-struct Rule(String, char);
+pub struct Rule(String, char);
+
+impl Rule {
+    pub fn new(s: &str, c: char) -> Self {
+        Self(s.to_string(), c)
+    }
+}
 
 impl FromStr for Rule {
     type Err = ();
@@ -54,6 +68,49 @@ impl FromStr for Rule {
             Err(())
         }
     }
+}
+
+pub fn apply(rules: &Vec<Rule>, polymer: Polymer) -> Polymer {
+    // Map rules
+    let mut table: HashMap<String, char> = HashMap::new();
+    for rule in rules {
+        table.insert(rule.0.clone(), rule.1.clone());
+    }
+
+    // Perform substitution
+    let s = polymer.to_string();
+    let left = s.chars();
+    let mut right = s.chars();
+    right.next();
+    let words: Vec<String> = left
+        .zip(right)
+        .map(|(a, b)| {
+            let mut word = String::new();
+            word.push(a);
+            word.push(b);
+            word
+        })
+        .collect();
+
+    let chars: Vec<char> = words
+        .iter()
+        .map(|w| table.get(w).unwrap())
+        .map(|c| *c)
+        .collect();
+
+    let n = s.len() + chars.len();
+    let mut t: String = String::with_capacity(n);
+    let mut polymer_letters = s.chars();
+    let mut insertion_letters = chars.iter();
+    for i in 0..n {
+        if i % 2 == 0 {
+            t.push(polymer_letters.next().unwrap());
+        } else {
+            t.push(*insertion_letters.next().unwrap());
+        }
+    }
+
+    Polymer(t)
 }
 
 #[cfg(test)]
@@ -76,6 +133,60 @@ mod tests {
     #[case("", Err(()))]
     fn parse_polymer(#[case] s: &str, #[case] expected: Result<Polymer, ()>) {
         let actual: Result<Polymer, _> = s.parse();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn perform_insertion() {
+        let polymer: Polymer = "NNCB".parse().unwrap();
+        let rules: Vec<Rule> = vec![
+            Rule::new("CH", 'B'),
+            Rule::new("HH", 'N'),
+            Rule::new("CB", 'H'),
+            Rule::new("NH", 'C'),
+            Rule::new("HB", 'C'),
+            Rule::new("HC", 'B'),
+            Rule::new("HN", 'C'),
+            Rule::new("NN", 'C'),
+            Rule::new("BH", 'H'),
+            Rule::new("NC", 'B'),
+            Rule::new("NB", 'B'),
+            Rule::new("BN", 'B'),
+            Rule::new("BB", 'N'),
+            Rule::new("BC", 'B'),
+            Rule::new("CC", 'N'),
+            Rule::new("CN", 'C'),
+        ];
+        let actual = apply(&rules, polymer);
+        let expected: Polymer = "NCNBCHB".parse().unwrap();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn perform_insertion_twice() {
+        let polymer: Polymer = "NNCB".parse().unwrap();
+        let rules: Vec<Rule> = vec![
+            Rule::new("CH", 'B'),
+            Rule::new("HH", 'N'),
+            Rule::new("CB", 'H'),
+            Rule::new("NH", 'C'),
+            Rule::new("HB", 'C'),
+            Rule::new("HC", 'B'),
+            Rule::new("HN", 'C'),
+            Rule::new("NN", 'C'),
+            Rule::new("BH", 'H'),
+            Rule::new("NC", 'B'),
+            Rule::new("NB", 'B'),
+            Rule::new("BN", 'B'),
+            Rule::new("BB", 'N'),
+            Rule::new("BC", 'B'),
+            Rule::new("CC", 'N'),
+            Rule::new("CN", 'C'),
+        ];
+        let actual = apply(&rules, apply(&rules, polymer));
+        let expected: Polymer = "NBCCNBBBCBHCB".parse().unwrap();
+
         assert_eq!(actual, expected);
     }
 }
