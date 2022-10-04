df4de2b306a6bfba4a8ffa4f0254f7fe1853d094
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-10-04 22:30:45 +0100

    play with layout

--- rust/snake/src/main.rs
+++ rust/snake/src/main.rs
@@ -6,7 +6,8 @@ use crossterm::terminal::{
 use std::io;
 use std::thread;
 use std::time::Duration;
-use tui::widgets::{Block, Borders};
+use tui::layout::{Alignment, Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Paragraph};
 use tui::{backend::CrosstermBackend, Terminal};
 
 fn main() -> Result<(), io::Error> {
@@ -19,9 +20,33 @@ fn main() -> Result<(), io::Error> {
     let mut terminal = Terminal::new(backend)?;
 
     terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default().title("Block").borders(Borders::ALL);
-        f.render_widget(block, size);
+        // Layout
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10),
+                ]
+                .as_ref(),
+            )
+            .split(f.size());
+
+        // Block 1
+        let block = Block::default().title("Block I").borders(Borders::ALL);
+        f.render_widget(block, chunks[0]);
+
+        // Block 2
+        let block = Block::default().title("Block II").borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
+
+        // Block 3
+        let block = Paragraph::new("Copyright 2022")
+            .alignment(Alignment::Center)
+            .block(Block::default().title("Block III").borders(Borders::ALL));
+        f.render_widget(block, chunks[2]);
     })?;
 
     // Wait 5 seconds
