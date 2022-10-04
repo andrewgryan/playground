1cc3ec4982c60c710f30c61305e47ecdeab48f83
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-10-04 22:03:36 +0100

    make a home made game

---
+++ rust/snake/src/main.rs
@@ -0,0 +1,40 @@
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use crossterm::execute;
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use std::io;
+use std::thread;
+use std::time::Duration;
+use tui::widgets::{Block, Borders};
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("Block").borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    // Wait 5 seconds
+    thread::sleep(Duration::from_millis(5000));
+
+    // Restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
