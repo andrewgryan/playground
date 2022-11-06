9e15f8d6f3ec045ba16df9b88e27767ca7b9bab0
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-11-06 14:08:11 +0000

    add a query with args

--- graphql-solid/server/server.js
+++ graphql-solid/server/server.js
@@ -7,6 +7,7 @@ var schema = buildSchema(`
   type Query {
     hello: String
     random: Float!
+    rollDice(numDice: Int!, numSides: Int): [Int!]!
   }
 `);
 
@@ -17,6 +18,13 @@ var root = {
   },
   random: () => {
     return Math.random();
+  },
+  rollDice: (args) => {
+    var output = [];
+    for (var i = 0; i < args.numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
+    }
+    return output;
   }
 };
 
