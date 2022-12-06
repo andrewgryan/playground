eb7cbd328e9407a7cb0de9813f9181d32a31eb7b
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-12-06 21:25:49 +0000

    solution to day 2 part 2

--- advent-of-code/2022/advent-2.hs
+++ advent-of-code/2022/advent-2.hs
@@ -14,6 +14,14 @@ fromChar c =
      'Y' -> Just Paper
      'Z' -> Just Scissors
      _ -> Nothing
+        
+roundFromChar :: Char -> Maybe Round
+roundFromChar c =
+   case c of
+     'X' -> Just Lose
+     'Y' -> Just Draw
+     'Z' -> Just Win
+     _ -> Nothing
     
 fromHands :: Hand -> Hand -> Round
 fromHands opponent you =
@@ -46,15 +54,39 @@ totalScore :: Hand -> Hand -> Int
 totalScore opponent you =
     roundScore (fromHands opponent you) + handScore you
 
-calculateScore :: String -> Int
-calculateScore s =
+calculatePart1Score :: String -> Int
+calculatePart1Score s =
     let
         opponent = fromJust (fromChar (head s))
         you = fromJust (fromChar (last s))
     in
     totalScore opponent you
 
+strategy :: Hand -> Round -> Hand
+strategy opponent round =
+    case (opponent, round) of
+        (Rock, Win) -> Paper
+        (Rock, Draw) -> Rock
+        (Rock, Lose) -> Scissors
+        (Paper, Win) -> Scissors
+        (Paper, Draw) -> Paper
+        (Paper, Lose) -> Rock
+        (Scissors, Win) -> Rock
+        (Scissors, Draw) -> Scissors
+        (Scissors, Lose) -> Paper
+        
+calculatePart2Score :: String -> Int
+calculatePart2Score s =
+    -- X, Y, Z maps to strategy
+    let
+        opponent = fromJust (fromChar (head s))
+        round = fromJust (roundFromChar (last s))
+        you = strategy opponent round
+    in
+    totalScore opponent you
+
 main = do
   text <- readFile "input-2"
   let rounds = lines text
-  print (sum (fmap calculateScore rounds))
+  print (sum (fmap calculatePart1Score rounds))
+  print (sum (fmap calculatePart2Score rounds))
