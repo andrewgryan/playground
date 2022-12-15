41ae363731117d2b912ab679d56848b6a5fd794b
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-12-15 11:26:25 +0000

    parse instructions

--- advent-of-code/2022/advent-5.hs
+++ advent-of-code/2022/advent-5.hs
@@ -1,15 +1,16 @@
 import Text.Read
-import Data.List (reverse, transpose)
+import Data.List (reverse, stripPrefix, transpose)
+import Data.List.Split (splitOn)
 
 data Crate = Crate Char deriving (Show)
 data Stack = Stack Char [Crate] deriving (Show)
-data Instruction = Instruction Int Int Int deriving (Show)
+data Instruction = Instruction String String String deriving (Show)
 
 toStack :: String -> Stack
 toStack s =
     case s of
         (x:xs) -> Stack x (((fmap Crate) . (filter notBlank)) xs)
-        _ -> Stack ' ' []
+        _ -> Stack ' ' [] -- Too lazy to do a Maybe Stack
 
 parseBoard :: String -> [Stack]
 parseBoard =
@@ -30,6 +31,22 @@ notBlank =
 isBlank :: Char -> Bool
 isBlank = (== ' ')
 
+
+toInstruction :: String -> Maybe Instruction
+toInstruction s =
+    let
+        words = splitOn " " s
+    in
+    case words of
+        ("move":x:"from":y:"to":z:_) ->
+            Just (Instruction x y z)
+        _ ->
+            Nothing
+
+parseInstructions :: String -> [Maybe Instruction]
+parseInstructions =
+    (fmap toInstruction) . lines
+
 main = do
     text <- readFile "input-5"
-    print (parseBoard text)
+    print (parseInstructions text)
