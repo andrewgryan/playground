2c857338a96362bb892668d63d3078b5bf4a4e1c
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-12-17 17:54:49 +0000

    solution day 6 part 2

--- advent-of-code/2022/advent-6.hs
+++ advent-of-code/2022/advent-6.hs
@@ -2,17 +2,27 @@ import qualified Data.Set as Set
 
 partOne :: String -> Int
 partOne s =
-   search s 0 + 4
+   let
+       width = 4
+   in
+   search width s 0 + width
     
-search :: String -> Int -> Int
-search s i =
+partTwo :: String -> Int
+partTwo s =
+   let
+       width = 14
+   in
+   search width s 0 + width
+    
+search :: Int -> String -> Int -> Int
+search w s i =
     let
-        chars = take 4 s
+        chars = take w s
     in
-    if length (Set.fromList chars) == 4 then
+    if length (Set.fromList chars) == w then
         i
     else
-        search (tail s) (i + 1)
+        search w (tail s) (i + 1)
 
 main = do
     text <- readFile "input-6"
@@ -21,3 +31,5 @@ main = do
     print (partOne "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
     print (partOne "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
     print (partOne text)
+    print (partTwo "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+    print (partTwo text)
