f52781aa37925afcee89041bb41c36e73a9e7a02
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-12-19 03:17:08 +0000

    solution to day 10 part 1

--- advent-of-code/2022/advent-10.hs
+++ advent-of-code/2022/advent-10.hs
@@ -8,27 +8,47 @@ data CPU = CPU Cycle Register deriving Show
 boot :: CPU
 boot =
   CPU 0 1
-  
-execute :: [Op] -> CPU -> CPU
-execute [] cpu =
-  cpu
-execute (op:ops) (CPU clock register) =
+
+advance :: Int -> ([Op], CPU) -> ([Op], CPU) 
+advance nticks (ops, cpu) =
+  if nticks == 0 then
+    (ops, cpu)
+  else
+    advance (nticks - 1) (advanceOnce (ops, cpu))
+
+advanceOnce :: ([Op], CPU) -> ([Op], CPU)
+advanceOnce ([], cpu) =
+  ([], tick cpu)
+advanceOnce (op:ops, cpu) =
   case op of
-    Noop ->
-      execute ops (CPU (clock + 1) register)
-    Addx n ->
-      execute ops (CPU (clock + 2) (register + n))
+    Noop _ ->
+      (ops, tick cpu)
+    Addx ticks n ->
+      if ticks == 0 then
+        advanceOnce (ops, store n cpu)
+      else if ticks == 1 then
+        (Addx 0 n:ops, tick cpu)
+      else
+        (Addx (ticks - 1) n:ops, tick cpu)
+  
+store :: Int -> CPU -> CPU
+store n (CPU clock register) =
+  CPU clock (register + n)
+
+tick :: CPU -> CPU
+tick (CPU clock register) =
+  CPU (clock + 1) register
 
 data Op
-  = Noop
-  | Addx Int
+  = Noop Int
+  | Addx Int Int
   deriving Show
 
 toOp :: String -> Maybe Op
 toOp s =
   case splitOn " " s of
-    ["noop"] -> Just Noop
-    ["addx", x] -> Just (Addx (read x :: Int))
+    ["noop"] -> Just (Noop 1)
+    ["addx", x] -> Just (Addx 2 (read x :: Int))
     _ -> Nothing
 
 smallProgram :: [String]
@@ -38,6 +58,20 @@ smallProgram =
   , "addx -5"
   ]
 
+signalStrength :: [Op] -> Int -> Int
+signalStrength ops cycle =
+  let
+    (_, cpu) = advance cycle (ops, boot)
+  in
+  strength cpu
+
+strength :: CPU -> Int
+strength (CPU cycle register) =
+  cycle * register
+
 main = do
-  let ops = Maybe.mapMaybe toOp smallProgram
-  print (execute ops boot)
+  text <- readFile "input-10"
+  let ops = Maybe.mapMaybe toOp (lines text)
+  let cycles = [20, 60, 100, 140, 180, 220]
+  let strengths = fmap (signalStrength ops) cycles
+  print (sum strengths)
