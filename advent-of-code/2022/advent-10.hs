776398201d8c97f0d74545627899d2954f348606
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-12-23 10:29:46 +0000

    solution to day 10 part 2

--- advent-of-code/2022/advent-10.hs
+++ advent-of-code/2022/advent-10.hs
@@ -1,13 +1,62 @@
-import Data.List.Split (splitOn)
+import Data.List.Split (splitOn, chunksOf)
 import qualified Data.Maybe as Maybe
 
 type Cycle = Int
 type Register = Int
 data CPU = CPU Cycle Register deriving Show
 
+type Width = Int
+data CRT = CRT Width [Pixel] deriving Show
+data Pixel = Lit | Dark deriving Show
+
+turnOnCRT :: CRT
+turnOnCRT =
+  CRT 40 []
+  
+toString :: CRT -> String
+toString (CRT w pxs) =
+  unlines (chunksOf w (fmap pxToChar pxs))
+  
+pxToChar :: Pixel -> Char
+pxToChar Lit = '#'
+pxToChar Dark = '.'
+ 
+draw :: CRT -> CPU -> CRT
+draw crt cpu =
+  if abs (crtX crt - cpuX cpu) <= 1 then
+    drawPixel Lit crt
+  else
+    drawPixel Dark crt
+    
+drawPixel :: Pixel -> CRT -> CRT
+drawPixel pixel (CRT w pixels) =
+  CRT w (pixel:pixels)
+
+cpuX :: CPU -> Int
+cpuX (CPU _ register) =
+  register
+  
+crtX :: CRT -> Int
+crtX (CRT width pixels) =
+  length pixels `mod` width
+
 boot :: CPU
 boot =
   CPU 0 1
+
+advanceWithCRT :: Int -> ([Op], CPU, CRT) -> ([Op], CPU, CRT) 
+advanceWithCRT nticks (ops, cpu, crt) =
+  if nticks == 0 then
+    (ops, cpu, crt)
+  else
+    advanceWithCRT (nticks - 1) (advanceOnceWithCRT (ops, cpu, crt))
+
+advanceOnceWithCRT :: ([Op], CPU, CRT) -> ([Op], CPU, CRT)
+advanceOnceWithCRT (ops, cpu, crt) =
+  let
+    (ops', cpu') = advanceOnce (ops, cpu)
+  in
+  (ops', cpu', draw crt cpu')
 
 advance :: Int -> ([Op], CPU) -> ([Op], CPU) 
 advance nticks (ops, cpu) =
@@ -69,8 +118,19 @@ strength :: CPU -> Int
 strength (CPU cycle register) =
   cycle * register
 
+main :: IO ()
 main = do
   text <- readFile "input-10"
+  partTwo text
+  
+partTwo :: String -> IO ()
+partTwo text = do
+  let ops = Maybe.mapMaybe toOp (lines text)
+  let (_, _, crt) = advanceWithCRT 240 (ops, boot, turnOnCRT)
+  putStr (toString crt)
+  
+partOne :: String -> IO ()
+partOne text = do
   let ops = Maybe.mapMaybe toOp (lines text)
   let cycles = [20, 60, 100, 140, 180, 220]
   let strengths = fmap (signalStrength ops) cycles
