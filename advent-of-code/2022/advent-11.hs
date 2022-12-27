c0db3ed9dd84a597888e17e905d03c33d7d29d4a
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-12-27 20:46:19 +0000

    parsed monkeys

--- advent-of-code/2022/advent-11.hs
+++ advent-of-code/2022/advent-11.hs
@@ -1,4 +1,6 @@
 import Control.Applicative
+import qualified Parsing
+import qualified Data.Maybe as Maybe
 
 -- type Operation = String
 -- type Item = Int
@@ -6,201 +8,88 @@ import Control.Applicative
 -- data Monkey = Monkey Int [Item] Operation Test
 
 
--- Hand-rolled parsing library
-
-newtype Parser a = P (String -> Maybe (a, String))
-
-instance Functor Parser where
-  -- fmap :: (a -> b) -> Parser a -> Parser b
-  fmap g p = P (\input ->
-    case parse p input of
-      Just (v, out) -> Just (g v, out)
-      Nothing -> Nothing
-    )
-    
-instance Applicative Parser where
-  -- pure :: a -> Parser a
-  pure v = P (\input -> Just (v, input))
-  
-  -- <*> :: Parser (a -> b) -> Parser a -> Parser b
-  pg <*> px = P (\input ->
-    case parse pg input of
-      Nothing -> Nothing
-      Just (g, out) -> parse (fmap g px) out)
-      
-instance Monad Parser where
-  -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-  p >>= f = P (\input ->
-    case parse p input of
-      Nothing -> Nothing
-      Just (v, out) -> parse (f v) out)
-
--- Choices
-instance Alternative Parser where
-  -- empty :: Parser a
-  empty = P (const Nothing)
-  
-  -- (<|>) :: Parser a -> Parser a -> Parser a
-  p <|> q = P (\input -> case parse p input of
-    Nothing -> parse q input
-    Just (v, out) -> Just (v, out)) 
-
-parse :: Parser a -> String -> Maybe (a, String)
-parse (P p) = p
-
-item :: Parser Char
-item = P (\input -> case input of
-  [] -> Nothing
-  (x:xs) -> Just (x, xs))
-
-satisfy :: (Char -> Bool) -> Parser Char
-satisfy p = do
-  x <- item
-  if p x then
-    return x
-  else
-    empty
-
-string :: String -> Parser String
-string [] = return []
-string (x:xs) = do
-  char x
-  string xs
-  return (x:xs)
-
-integer :: Parser Int
-integer = do
-  char '-'
-  n <- nat
-  return (-n)
-  <|> nat
-
-nat :: Parser Int
-nat = do
-  xs <- some digit
-  return (read xs)
-
-digit :: Parser Char
-digit = satisfy isDigit
-
-isDigit :: Char -> Bool
-isDigit c =
-    case c of
-      '0' -> True
-      '1' -> True
-      '2' -> True
-      '3' -> True
-      '4' -> True
-      '5' -> True
-      '6' -> True
-      '7' -> True
-      '8' -> True
-      '9' -> True
-      _ -> False
-
-char :: Char -> Parser Char
-char c = satisfy (== c)
-
-newline :: Parser ()
-newline = do
-  char '\n'
-  return ()
-
--- Handling space
-token :: Parser a -> Parser a
-token p = do
-  space
-  v <- p
-  space
-  return v
-
-space :: Parser ()
-space = do
-  many (satisfy isSpace)
-  return ()
-
-isSpace :: Char -> Bool
-isSpace = (== ' ')
-
-
--- Equations
-
-data Expression = Value Int | Binary Expression Char Expression deriving Show
-data Equation = Equation String Expression deriving Show
-
-equation :: Parser Equation
-equation = do
-  name <- identifier
-  space
-  char '='
-  space
-  Equation name <$> expression
-
-expression :: Parser Expression
-expression = do binary <|> (Value <$> integer)
-
-value :: Parser Expression
-value =
-  Value <$> integer
-
-binary :: Parser Expression
-binary = do
-  lhs <- expression
-  space
-  op <- operation
-  space
-  rhs <- expression
-  return (Binary lhs op rhs)
-  
-operation :: Parser Char
-operation =
-  satisfy isOp
-
-isOp :: Char -> Bool
-isOp c =
-  case c of
-    '+' -> True
-    '-' -> True
-    '*' -> True
-    '/' -> True
-    _ -> False
-
-identifier :: Parser String
-identifier = token ident
-
-ident :: Parser String
-ident = string "old" <|> string "new"
-
 -- Monkey parser
+data Monkey = Monkey
+  { index :: Int
+  , items :: [Int]
+  , operation :: String
+  , test :: String
+  , ifTrue :: String
+  , ifFalse :: String
+  } deriving Show
 
-data Monkey = Monkey Int [Int] deriving Show
-
-startingItems :: Parser [Int]
+startingItems :: Parsing.Parser [Int]
 startingItems = do
-  string "Starting items:"
-  space
+  Parsing.string "Starting items:"
+  Parsing.space
   integers
 
-integers :: Parser [Int]
+integers :: Parsing.Parser [Int]
 integers = do
-  x <- integer
-  xs <- some (do
-      space
-      char ','
-      space
-      integer)
+  x <- Parsing.integer
+  xs <- many (do
+      Parsing.space
+      Parsing.char ','
+      Parsing.space
+      Parsing.integer)
   return (x:xs)
+  
+monkeyId :: Parsing.Parser Int
+monkeyId = do
+  Parsing.string "Monkey"
+  Parsing.space
+  n <- Parsing.nat
+  Parsing.char ':'
+  return n
+  
+newlineSeparated :: Parsing.Parser a ->  Parsing.Parser [a]
+newlineSeparated p = do
+  x <- p
+  xs <- some (do
+    some (Parsing.char '\n')
+    p)
+  return (x:xs)
+  
 
-monkey :: Parser Monkey
+monkey :: Parsing.Parser Monkey
 monkey = do
-  string "Monkey"
-  space
-  n <- nat
-  char ':'
-  newline
-  space
+  id <- monkeyId
+  Parsing.newline
+  Parsing.space
   items <- startingItems
-  return (Monkey n items)
+  Parsing.newline
+  operation' <- keyword "Operation:"
+  Parsing.newline
+  test' <- keyword "Test:"
+  Parsing.newline
+  ifTrue' <- keyword "If true:"
+  Parsing.newline
+  ifFalse' <- keyword "If false:"
+  Parsing.newline
+  return Monkey
+    { index = id
+    , items = items
+    , operation = operation'
+    , test = test'
+    , ifTrue = ifTrue'
+    , ifFalse = ifFalse'
+    }
+    
+
+keyword :: String -> Parsing.Parser String
+keyword key = do
+  Parsing.space
+  Parsing.string key
+  Parsing.space
+  chars
+  
+chars :: Parsing.Parser String
+chars =
+  many (Parsing.satisfy notEOL)
+  
+notEOL :: Char -> Bool
+notEOL =
+  (/= '\n')
 
 -- Puzzle
 example :: String
@@ -223,4 +112,6 @@ example = unlines
 
 main :: IO ()
 main = do
-  putStrLn example
+  text <- readFile "input-11"
+  let (monkeys, left) = Maybe.fromJust (Parsing.parse (newlineSeparated monkey) text)
+  print monkeys
