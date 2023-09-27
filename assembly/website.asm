147c5cb08e8783363d70f518205d4ca7720fe416
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-09-27 23:52:04 +0100

    bind a socket in assembly

--- assembly/website.asm
+++ assembly/website.asm
@@ -3,9 +3,12 @@ format ELF64 executable
 SYS_write = 1
 SYS_exit = 60
 SYS_socket = 41
+SYS_bind = 49
 
 AF_INET = 2
 SOCK_STREAM = 1
+INADDR_ANY = 0
+PORT = 14619 ;; 6969
 
 STDOUT = 1
 STDERR = 2
@@ -39,6 +42,21 @@ macro socket domain, type, protocol
     syscall
 }
 
+macro syscall3 number, a, b, c
+{
+    mov rax, number
+    mov rdi, a
+    mov rsi, b
+    mov rdx, c
+    syscall
+}
+
+;; int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
+macro bind sockfd, addr, addrlen
+{
+    syscall3 SYS_bind, sockfd, addr, addrlen
+}
+
 segment readable executable
 entry main
 main:
@@ -48,7 +66,16 @@ main:
     socket AF_INET, SOCK_STREAM, 0
     cmp rax, 0
     jl error
-    mov dword [sockfd], eax
+    mov qword [sockfd], rax
+    write STDOUT, ok_msg, ok_msg_len
+
+    write STDOUT, bind_trace_msg, bind_trace_msg_len
+    mov word [servaddr.sin_family], AF_INET
+    mov dword [servaddr.sin_addr], INADDR_ANY
+    mov word [servaddr.sin_port], PORT
+    bind [sockfd], servaddr.sin_family, sizeof_servaddr
+    cmp rax, 0
+    jl error
     write STDOUT, ok_msg, ok_msg_len
 
     exit EXIT_SUCCESS
@@ -62,12 +89,27 @@ error:
 ;; dd - 4 byte
 ;; dq - 8 byte
 segment readable writeable
-sockfd dd 0
+sockfd dq 0
+servaddr.sin_family dw 0
+servaddr.sin_port dw 0
+servaddr.sin_addr dd 0
+servaddr.sin_zero dq 0
+sizeof_servaddr = $ - servaddr.sin_family
+
 start db "INFO: Starting Web-site!", 10
 start_len = $ - start
 error_msg db "ERROR!", 10
 error_msg_len = $ - error_msg
 socket_trace_msg db "INFO: Starting a socket...", 10
 socket_trace_msg_len = $ - socket_trace_msg
+bind_trace_msg db "INFO: Bind the socket...", 10
+bind_trace_msg_len = $ - bind_trace_msg
 ok_msg db "INFO: OK", 10
 ok_msg_len = $ - ok_msg
+
+;; struct sockaddr_in {
+;;     family 16 bits
+;;     port   16 bits
+;;     addr   32 bits
+;;     zero[8] 64bits
+;; }
