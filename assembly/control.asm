827e4b2863e1e7b6b6e86dd1e55c848a9619cfad
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-10-02 00:29:30 +0100

    estimate string length

--- assembly/control.asm
+++ assembly/control.asm
@@ -8,17 +8,36 @@ SUCCESS = 0
 segment readable executable
 entry main
 main:
-    ;; Raw asm
-    mov [buf], 0x21
-    mov [buf+1], 0x0a
-
-    write STDOUT, buf, 2
+    ;; calculate string length dynamically
+    mov rdi, msg
+    call strlen
+    mov rdx, rax
+    write STDOUT, msg, rdx
     
     exit SUCCESS
 
+strlen:
+    ;; push registers
+    push rdi
+    xor rax, rax ;; zero rax register
+
+.next_char:
+    mov al, byte [rdi]  ;; move 1 byte into the lower part of the rax register
+    cmp rax, 0 ;; check byte is zero
+    je .done
+
+    inc rdi ;; Increment pointer
+    jmp .next_char
+
+.done:
+    ;; 0-byte encountered
+    pop rsi
+    sub rdi, rsi
+    mov rax, rdi
+    ret
+
+
 segment readable writeable
 
-buf rb 2
-
-msg db "Hello, World!", 10
+msg db "Hello, World!", 10, 0
 msg_len = $ - msg
