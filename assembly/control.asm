ead565103dca4fcb82e28247e3d2b7723f926d71
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-10-01 18:13:20 +0100

    print a character

--- assembly/control.asm
+++ assembly/control.asm
@@ -8,13 +8,16 @@ SUCCESS = 0
 segment readable executable
 entry main
 main:
-    write STDOUT, msg, msg_len
     ;; Raw asm
-    xor rax, rax
-    mov rbx, 42
+    mov [buf], 0x21
+
+    write STDOUT, buf, 1
     
-    exit rbx
+    exit SUCCESS
 
 segment readable writeable
+
+buf rb 1
+
 msg db "Hello, World!", 10
 msg_len = $ - msg
