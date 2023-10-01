ff292361fd31d486135d9092149bb70b16493dd2
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-10-01 12:27:45 +0100

    implement hello world again

--- assembly/control.asm
+++ assembly/control.asm
@@ -8,4 +8,9 @@ SUCCESS = 0
 segment readable executable
 entry main
 main:
+    write STDOUT, msg, msg_len
     exit SUCCESS
+
+segment readable writeable
+msg db "Hello, World!", 10
+msg_len = $ - msg
