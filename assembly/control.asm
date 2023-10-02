f94da688b3fab88bbdef1b4ad13579ecf42b278c
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-10-02 18:08:55 +0100

    improve startswith function

--- assembly/control.asm
+++ assembly/control.asm
@@ -29,15 +29,26 @@ main:
     ;; startswith "GET "
     mov rdi, http_message
     mov rsi, http_get
+    mov rdx, http_get_len
     call startswith
     cmp rax, 0
     je get
 
+    ;; startswith "POST "
     mov rdi, http_message
     mov rsi, http_post
+    mov rdx, http_post_len
     call startswith
     cmp rax, 0
     je post
+
+    ;; startswith "DELETE "
+    mov rdi, http_message
+    mov rsi, http_delete
+    mov rdx, http_delete_len
+    call startswith
+    cmp rax, 0
+    je delete
 
     jmp error
     
@@ -51,18 +62,48 @@ post:
     write STDOUT, post_msg, post_msg_len
     exit SUCCESS
 
+delete:
+    write STDOUT, delete_msg, delete_msg_len
+    exit SUCCESS
+
 error:
     write STDOUT, error_msg, error_msg_len
     exit FAILURE
 
 startswith:
-    xor rax, rax
+    xor rax, rax  ;; Zero rax register
 
-    ;; Algorithm
+.step:
+    ;; Check there are bytes left to process
+    cmp rdx, 0
+    je .succeed
+
+    ;; Load next pair of bytes
     mov al, byte [rdi]
     mov bl, byte [rsi]
 
-    sub al, bl  ;; updates lower part of rax register
+    ;; If prefix terminated exit with succeed
+    cmp bl, 0
+    je .succeed
+
+    ;; Compare bytes
+    cmp al, bl  ;; updates lower part of rax register
+    jne .fail
+
+    ;; Update indices
+    inc rdi
+    inc rsi
+    dec rdx
+
+    ;; Loop while bytes compare successfully
+    jmp .step
+
+.fail:
+    mov rax, 1
+    ret
+
+.succeed:
+    mov rax, 0
     ret
 
 strlen:
@@ -132,10 +173,16 @@ get_msg db "GET detected!", 10, 0
 get_msg_len = $ - get_msg
 post_msg db "POST detected!", 10, 0
 post_msg_len = $ - post_msg
+delete_msg db "DELETE detected!", 10, 0
+delete_msg_len = $ - delete_msg
 error_msg db "Error detected!", 10, 0
 error_msg_len = $ - error_msg
 
 ;; Example HTTP message
-http_message db "POST /favicon.ico", 10, 0
+http_message db "DELETE /favicon.ico", 10, 0
 http_get db "GET ", 0
+http_get_len = $ - http_get
 http_post db "POST ", 0
+http_post_len = $ - http_post
+http_delete db "DELETE ", 0
+http_delete_len = $ - http_delete
