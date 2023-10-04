6bbc2fb548fb625d51155fd9dd281a1744e4dc79
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-10-04 18:11:47 +0100

    implement uitoa

--- assembly/itoa.asm
+++ assembly/itoa.asm
@@ -40,8 +40,7 @@ itoa:
     ;; TODO calculate length of buffer taken up by number
     xor r8, r8
 
-    ;; Division
-    xor eax, eax ;; clear dividend
+    ;; Detect number of chars
     xor edx, edx ;; clear dividend
     mov eax, esi ;; dividend
 
@@ -55,16 +54,35 @@ itoa:
     jnz .loop
     
 
-    ;; TODO fill buffer with ASCII characters
-
-
-    ;; Print loop counter
-    add r8b, 48  ;; convert digit to ASCII
-    mov byte [rdi], r8b
+    ;; Fill buffer with ASCII characters
+    xor edx, edx ;; clear dividend
+    mov eax, esi ;; dividend
 
     ;; NULL terminate
-    inc rdi
-    mov byte [rdi], 0
+    inc r8
+    mov byte [rdi + r8], 0
+
+    ;; Reset digit counter
+    xor r9, r9
+    xor r10, r10
+.fill:
+    dec r8 ;; decrement digit counter
+
+    ;; Divide number by 10
+    xor edx, edx ;; clear dividend
+    mov ecx, 0x0a ;; divisor
+    div ecx ;; perform unsigned division
+
+    ;; Write ASCII character to buffer
+    mov r9d, eax ;; Save division
+    mov r10d, edx ;; Save remainder
+    add r10d, 48 ;; convert remainder to ASCII
+    mov byte [rdi+r8], r10b ;; place digit in buffer
+
+    ;; Loop condition
+    cmp r9d, 0 ;; check division non-zero 
+    jnz .fill
+
 
     ret
 
