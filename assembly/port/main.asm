7e822b7d551fe826960d4e5927305ed9b87066a3
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-10-21 19:19:56 +0100

    add support for printing numbers

--- assembly/port/main.asm
+++ assembly/port/main.asm
@@ -2,22 +2,55 @@ format ELF64 executable
 ;; Convert PORT number from decimal to network endian decimal
 ;;
 ;; Algorithm
-;; 1. Convert from decimal to hexadecimal
-;; 2. Reverse the bytes
-;; 3. Convert back into decimal
+;; ---------
+;; 1. Reverse the bytes
 ;;
 include "lib.inc"
+
+CARRIAGE_RETURN = 10
 
 segment readable executable
 entry main
 main:
-   ;; Print Hello, World!
+   ;; Put 1 in rdx buffer and bit shift left
+   mov rdx, 1
+   shl edx, 8
+
+   ;; Integer to ASCII
+   fn itoa, buf, rdx
+
+   ;; Add carriage return
+   fn append, buf, CARRIAGE_RETURN
+   
+   ;; Print buffer
    print buf, len
 
    ;; Exit with return code
-   exit 42
+   exit 0
    
+;; Append
+;; Scan string for 0, replace with char and append 0
+;; @param {*text} - rdi
+;; @param {char} - rsi
+;; @returns {*text} - rax
+append:
+    push rdi     ;; Save buf location on stack
+    xor rax, rax ;; Reset buffer to zero
+
+.next:
+    mov al, byte[rdi] ;; Read byte into rax
+    cmp rax, 0        ;; Check is zero
+    je .done
+
+    inc rdi ;; Move to next character
+    jmp .next
+
+.done:
+    mov byte [rdi], sil   ;; Write rsi char to rdi
+    mov byte [rdi + 1], 0 ;; Append 0
+    pop rdi               ;; Restore original pointer
+    ret
 
 segment readable writable
-buf db "Hello, World!", 10
+buf db "    ", CARRIAGE_RETURN
 len = $ - buf
