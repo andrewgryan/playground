4151a0bf2badde21b4785b38412517767a439107
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-11-13 18:04:09 +0000

    further simplification

--- assembly/port/hello.asm
+++ assembly/port/hello.asm
@@ -48,22 +48,20 @@ main:
 	; echo [rbp + 16], [rbp + 24] 	; Echo arg 2
 
 	; Stack-based str
-	lea rdi, [rbp + 32]
-	mov [rdi + 0], byte 0x41
-	mov [rdi + 1], byte 0x6E
-	mov [rdi + 2], byte 0x64
-	mov [rdi + 3], byte 0x79
-	mov [rdi + 4], byte 0x20
-	mov [rdi + 5], byte 0x72
-	mov [rdi + 6], byte 0x75
-	mov [rdi + 7], byte 0x6C
-	mov [rdi + 8], byte 0x65
-	mov [rdi + 9], byte 0x73
-	mov [rdi + 10], byte 0x21
-	mov [rdi + 11], byte 0xa
-	mov [rdi + 12], byte 0x0
-	mov r10, rbp
-	add r10, 32
+	lea r10, [rbp + 32]
+	mov [r10 + 0], byte 0x41
+	mov [r10 + 1], byte 0x6E
+	mov [r10 + 2], byte 0x64
+	mov [r10 + 3], byte 0x79
+	mov [r10 + 4], byte 0x20
+	mov [r10 + 5], byte 0x72
+	mov [r10 + 6], byte 0x75
+	mov [r10 + 7], byte 0x6C
+	mov [r10 + 8], byte 0x65
+	mov [r10 + 9], byte 0x73
+	mov [r10 + 10], byte 0x21
+	mov [r10 + 11], byte 0xa
+	mov [r10 + 12], byte 0x0
 	call1 strlen, r10
 	mov r9, rax
 	echo r10, r9
