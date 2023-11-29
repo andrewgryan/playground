96877f5914d6cafb31f7c70af79e763ee89eeaf4
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-11-29 20:27:17 +0000

    heap allocate string

--- assembly/port/test_brk.asm
+++ assembly/port/test_brk.asm
@@ -2,8 +2,10 @@ format ELF64 executable
 
 ; Example demonstrating dynamic allocation of data
 
+SYS_write = 1
 SYS_exit = 60
 SYS_brk = 0x0c
+STDOUT = 1
 
 segment readable executable
 entry main
@@ -17,14 +19,26 @@ main:
         mov r8, rax
 
         ; Ask for a new breakpoint
-        lea rdi, [rax + 2]
+        lea rdi, [rax + 3]
         mov rax, SYS_brk
         syscall
 
-        ; Subtract breakpoint locations
-        sub r8, rax
+        ; Save breakpoint location
+        mov r10, rax
+
+        ; Write a character into the allocated address
+        mov [r10], byte 49
+        mov [r10 + 1], byte 50
+        mov [r10 + 2], byte 10
+
+        ; Print
+        mov rax, SYS_write
+        mov rdi, STDOUT
+        mov rsi, r10
+        mov rdx, 3
+        syscall
 
         ; Exit system call
         mov rax, SYS_exit
-        mov rdi, r8
+        mov rdi, 0
         syscall
