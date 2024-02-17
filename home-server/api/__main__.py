911f0bc4e4980925d113aa86bc01becd4b6b2c2d
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-02-17 18:55:14 +0000

    .

--- home-server/api/__main__.py
+++ home-server/api/__main__.py
@@ -1,27 +1,71 @@
+import sqlite3
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 LIKES = 0
 
+
 class Item(BaseModel):
     id: str
+
 
 class Likes(BaseModel):
     likes: int
 
-app = FastAPI()
+
+connection = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global connection
+    connection = sqlite3.connect("blog.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS
+            likes (id INTEGER UNIQUE, like INTEGER);
+        """
+    )
+    yield
+    connection.commit()
+    connection.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 async def root() -> Item:
     return Item(id="Andy")
 
+
 @app.post("/like")
-async def likes() -> Likes:
-    global LIKES
-    LIKES += 1
-    return Likes(likes=LIKES)
+async def post_likes() -> Likes:
+    global connection
+    cursor = connection.cursor()
+    result = cursor.execute("SELECT like FROM likes WHERE id=1;")
+    rows = result.fetchall()
+    if len(rows) == 0:
+        likes = 0
+    else:
+        likes = rows[0][0]
+    likes += 1
+    cursor.execute("INSERT OR REPLACE INTO likes (id, like) VALUES (1, ?);", (likes,))
+    connection.commit()
+    return Likes(likes=likes)
+
 
 @app.get("/like")
-async def likes() -> Likes:
-    global LIKES
-    return Likes(likes=LIKES)
+async def get_likes() -> Likes:
+    global connection
+    cursor = connection.cursor()
+    result = cursor.execute("SELECT like FROM likes WHERE id=1;")
+    rows = result.fetchall()
+    if len(rows) == 0:
+        likes = 0
+    else:
+        likes = rows[0][0]
+    return Likes(likes=likes)
--- home-server/front/__main__.py
+++ home-server/front/__main__.py
@@ -1,23 +1,28 @@
 import requests
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+API_SERVER = "http://localhost:8000"
+
+
 @app.route("/")
 def index():
-    response = requests.get("http://localhost:8000")
+    response = requests.get(f"{API_SERVER}")
     name = response.json()["id"]
     style_href = url_for("static", filename="style.css")
     return render_template("index.html", name=name, style_href=style_href)
 
+
 @app.route("/like", methods=["POST"])
 def like():
-    response = requests.post("http://localhost:8000/like")
+    response = requests.post(f"{API_SERVER}/like")
     likes = response.json()["likes"]
     return render_template("likes.html", likes=likes)
 
+
 @app.route("/likes")
 def likes():
-    response = requests.get("http://localhost:8000/like")
+    response = requests.get(f"{API_SERVER}/like")
     likes = response.json()["likes"]
     return render_template("likes.html", likes=likes)
